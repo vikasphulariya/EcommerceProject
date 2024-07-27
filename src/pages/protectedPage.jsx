2b8a0612aa2b2bdea1bac762a01d8ddceded4510
Vikas Phulriya <vikasphulariya@gmail.com>
2024-07-27 22:37:22 +0530

    fixed typo

--- src/pages/protectedPage.jsx
+++ src/pages/protectedPage.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import  { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import AuthNeeded from "./AuthNeeded"; // Adjust the path as needed
+import { ClipLoader } from "react-spinners";
 // import ComponentA from "./ComponentA"; // Adjust the path as needed
 
 function ProtectedPage({ children }) {
   const isLoggedIn = useSelector((state) => state.user.user);
-  uss
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, []);
+  if (loading)
+    return (
+      <>
+        <div className="  w-full grid place-items-center">
+          <ClipLoader color="#36d7b7" size={50} />
+        </div>
+      </>
+    );
 
   return isLoggedIn ? <> {children}</> : <AuthNeeded />;
 }
